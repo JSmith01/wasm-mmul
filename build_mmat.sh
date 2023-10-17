402a46195d25075a055d948d8d0e98a91e873252
Dmitry Zlygin <dzlygin@gmail.com>
2023-10-17 14:56:39 -0400

    added multithreaded calculation

--- build_mmat.sh
+++ build_mmat.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 
-emcc -O3 -Oz -msimd128 -sALLOW_MEMORY_GROWTH -s EXPORT_NAME=loadWasmMmul -sEXPORTED_FUNCTIONS=_alloc_buf,_free_buf,_mmul,_mmul_simd mmul.c -o mmul.mjs
+emcc -O3 -msimd128 -sALLOW_MEMORY_GROWTH -s EXPORT_NAME=loadWasmMmul -sEXPORTED_FUNCTIONS=_alloc_buf,_free_buf,_mmul,_mmul_simd mmul.c -o mmul.mjs
